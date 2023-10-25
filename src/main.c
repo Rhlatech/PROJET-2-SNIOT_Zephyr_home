046830126042631d1fa5e429a71bf8a34a785b94
Billal Mouhtadi <billal.mouhtadi.25@seatech.fr>
2023-10-25 17:06:06 +0200

    Allumer LED

--- src/main.c
+++ src/main.c
@@ -5,9 +5,17 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+
+const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD);
+	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
+	printf("On est passé");
 	return 0;
 }
